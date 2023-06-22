f8e2d2b9b19e90cfccb245d81a5ee9048c6107a3
lordvadr <lordvadr@futurology.social>
2023-06-22 11:21:42 -0500

    Adds last signed commit finding logic to library.

--- setup.d/library.sh
+++ setup.d/library.sh
@@ -16,6 +16,28 @@ cleanup() {
 	unset TMPDIR
 }
 
+last_signed_commit() {
+	local dir=0
+	[ -z "${2:-}" ] || [ ! -d "${2}" ] || { dir=1; pushd "${2}" > /dev/null 2>&1 || die "Could not change directory to \"${2}\"."; } 
+
+	[ -z "${1:-}" ] || git checkout "${1}" || die "Could not checkout branch \"${1}\"."
+
+	COMMIT=""
+
+	for c in $(git log --pretty=format:"%H"); do
+		if git verify-commit "${c}"; then
+			COMMIT="${c}"
+			break
+		else
+			COMMIT=""
+		fi
+	done
+
+	[ "${dir}" != "1" ] || popd > /dev/null 2>&1
+
+	[ -n "${COMMIT}" ] && echo "${COMMIT}"
+}
+
 [ -z "${DEBUG+x}" ] || { log "DEBUG environment variable is set. Enabling debugging output."; set -x; }
 
 trap cleanup EXIT
