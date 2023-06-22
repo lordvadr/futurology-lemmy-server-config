8504f55c74a6875970a6be2496f9955c7f59f3b2
lordvadr <lordvadr@futurology.social>
2023-06-22 11:10:13 -0500

    Adds cleanup function to library.

--- setup.d/library.sh
+++ setup.d/library.sh
@@ -11,4 +11,11 @@ die() {
 	exit 1
 }
 
+cleanup() {
+	[ -z "${TMPDIR:=}" ] || [ ! -d "${TMPDIR}" ] || rm -rf "${TMPDIR}"
+	unset TMPDIR
+}
+
 [ -z "${DEBUG+x}" ] || { log "DEBUG environment variable is set. Enabling debugging output."; set -x; }
+
+trap cleanup EXIT
