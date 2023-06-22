c079238789ceaafe142aa377192dbd942307a1f8
lordvadr <lordvadr@futurology.social>
2023-06-22 11:11:28 -0500

    Adds error message to hostnamectl command

--- setup.d/05-hostname.sh
+++ setup.d/05-hostname.sh
@@ -2,4 +2,4 @@
 
 . "$(dirname "$(realpath "${0}")")/library.sh" || { >&2 echo "FATAL: Could not instantiate function library."; exit 1; }
 
-hostnamectl hostname futurology.social
+hostnamectl hostname futurology.social || die "Could not set hostname."
