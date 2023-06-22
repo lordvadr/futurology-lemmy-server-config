8adf2c1f74cb1f447360f0a72e13c1142d076116
lordvadr <lordvadr@futurology.social>
2023-06-22 11:23:25 -0500

    Now processes users from users file.

--- setup.d/10-ec2-user.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-id ec2-user > /dev/null 2>&1 && userdel -r ec2-user
-id rocky > /dev/null 2>&1 && userdel -r rocky
---
+++ setup.d/10-users.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+. "$(dirname "$(realpath "${0}")")/library.sh" || { >&2 echo "FATAL: Could not instantiate function library."; exit 1; }
+
+TMPDIR="$(mktemp -d /tmp/${0}.XXXXXXXX)" || die "Could not create temporary directory."
+
+git clone "https://github.com/lordvadr/futurology-ssh-keys.git" "${TMPDIR}" || die "Could not clone ssh users repository."
+
+pushd "${TMPDIR}" || die "Could not change working directory to \"${TMPDIR}\"."
+
+commit="$(last_signed_commit)"
+
+[ -n "${commit}" ] || die "No signed commit found."
+
+git checkout "${commit}" || die "Could not checkout commit \"${commit}\"."
+
+./install.sh
