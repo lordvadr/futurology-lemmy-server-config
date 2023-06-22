19b50a72b4b4501ba80fbf4100584fee1f498788
lordvadr <lordvadr@futurology.social>
2023-06-22 11:46:10 -0500

    Fixed buggy tmpdir creation.

--- setup.d/10-users.sh
+++ setup.d/10-users.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 . "$(dirname "$(realpath "${0}")")/library.sh" || { >&2 echo "FATAL: Could not instantiate function library."; exit 1; }
 
-TMPDIR="$(mktemp -d /tmp/${0}.XXXXXXXX)" || die "Could not create temporary directory."
+TMPDIR="$(mktemp -d /tmp/$(basename "${0}").XXXXXXXX)" || die "Could not create temporary directory."
 
 git clone "https://github.com/lordvadr/futurology-ssh-keys.git" "${TMPDIR}" || die "Could not clone ssh users repository."
 
